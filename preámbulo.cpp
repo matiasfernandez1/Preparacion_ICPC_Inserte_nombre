e73782c68508745a88833b3ea1cafee0db664be6
matiasfernandez1 <matiasfernandeztaipe@gmail.com>
2020-05-28 19:32:01 -0400

    preámbulo

---
+++ preámbulo.cpp
@@ -0,0 +1,43 @@
+#pragma GCC optimize("Ofast")
+#include <bits/stdc++.h>
+using namespace std;
+
+#define OJ                            \
+    freopen("input.txt", "r", stdin); \
+    freopen("output.txt", "w", stdout);
+#define FIO                           \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);                   \
+    cout.precision(4);
+
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+typedef long long ll;
+#define loop(h)  for(int i=0; i<h; i++)
+#define REP(i,k,n) for (i = k; i <= n; ++i) 
+#define REPR(i,k,n) for (i = k; i >= n; --i) 
+#define all(v) v.begin(), v.end()
+#define PB push_back
+#define FF first
+#define SS second
+#define what_is(x) cout << #x << " is " << x << "\n";
+#define end cout << "\n";
+
+template<typename... T>
+void read(T&... args) {
+    ((cin >> args), ...);
+}
+
+template<typename... T>
+void write(T&&... args) { 
+    ((cout << args << " "), ...);
+}
+
+int main(){
+    OJ;
+    FIO;
+
+    
+    return 0;
+}
